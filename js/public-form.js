5962ce8d17993254074e85393a4b17e9248f84d0
Josh Hallinan <jhallinan@fanatics.com>
2021-06-16 10:27:05 -0600

    add race checkboxes and validation

--- js/public-form.js
+++ js/public-form.js
@@ -1,16 +1,15 @@
 (function ($) {
+	$("#bia-form-phone").usPhoneFormat({
+		format: "xxx-xxx-xxxx",
+	});
 
-  $("#bia-form-phone").usPhoneFormat({
-    format: 'xxx-xxx-xxxx'
-  })
-
-  new AutoNumeric("#bia-form-monthly-income", {
+	new AutoNumeric("#bia-form-monthly-income", {
 		currencySymbol: "$",
 		roundingMethod: "S",
 		decimalPlaces: 0,
 		decimalPlacesRawValue: 0,
 		minimumValue: 0,
-  });
+	});
 
 	$.validator.addMethod(
 		"checkBirthday",
@@ -44,8 +43,8 @@
 			},
 			phone: {
 				required: true,
-        minlength: 10,
-        phoneUS: true,
+				minlength: 10,
+				phoneUS: true,
 			},
 			email: {
 				required: false,
@@ -55,41 +54,44 @@
 			},
 			address: {
 				required: true,
-      },
-      apartment_num: {
-        required: false
-      },
-      city: {
-        required: true,
-      },
+			},
+			apartment_num: {
+				required: false,
+			},
+			city: {
+				required: true,
+			},
 			zip: {
 				required: true,
 				minlength: 5,
 				maxlength: 5,
 				digits: true,
-      },
-      county: {
-        required: true,
-      },
+			},
+			county: {
+				required: true,
+			},
+			race: {
+				required: true,
+			},
 			num_in_house: {
 				required: true,
 				digits: true,
-      },
-      is_disabled: {
-        required: true,
-      },
-      is_over_sixty: {
-        required: true,
-      },
+			},
+			is_disabled: {
+				required: true,
+			},
+			is_over_sixty: {
+				required: true,
+			},
 			monthly_income: {
 				required: true,
-      },
-      is_able_to_text: {
-        required: true
-      },
-      special_instructions: {
-        required: false
-      }
+			},
+			is_able_to_text: {
+				required: true,
+			},
+			special_instructions: {
+				required: false,
+			},
 		},
 		messages: {
 			first_name: "This field is required",
@@ -97,9 +99,37 @@
 			phone: "Please enter a valid phone",
 			email: "please enter a valid email",
 			address: "Enter a valid address",
-      zip: "Enter a valid zip",
-      county: "Please choose an option",
-      num_in_house: "Please Enter a Number 1-8",
+			zip: "Enter a valid zip",
+			county: "Please choose an option",
+			num_in_house: "Please Enter a Number 1-8",
+		},
+		errorPlacement: function (error, element) {
+			if (error.is("#race-error")) {
+				error.appendTo("#bia-form-race-error");
+			} else error.insertAfter(element);
+		},
+		highlight: function (element, errorClass, validClass) {
+			console.log("element", element);
+			if ($(element).is('[name="race"]')) {
+				$('[name="race"]').addClass(errorClass);
+			} else if (element.type === "radio") {
+				this.findByName(element.name)
+					.addClass(errorClass)
+					.removeClass(validClass);
+			} else {
+				$(element).addClass(errorClass).removeClass(validClass);
+			}
+		},
+		unhighlight: function (element, errorClass, validClass) {
+			if ($(element).is('[name="race"]')) {
+				$('[name="race"]').removeClass(errorClass);
+			} else if (element.type === "radio") {
+				this.findByName(element.name)
+					.removeClass(errorClass)
+					.addClass(validClass);
+			} else {
+				$(element).removeClass(errorClass).addClass(validClass);
+			}
 		},
 		submitHandler: function (form) {
 			var params = $(form).serialize();
